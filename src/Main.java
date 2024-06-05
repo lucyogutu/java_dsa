173006f67f6b435df8e0d31dbd86e91bbb1bbf12
lucyogutu <amondilucy192@gmail.com>
2024-06-05 08:20:21 +0300

    check palindrome

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+
+        String s = "racecar";
+        String x = "acdea";
+
+        System.out.println(Palindrome.checkIfPalindrome(s));
+
+    }
+
+}
